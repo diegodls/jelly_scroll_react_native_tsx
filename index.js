90aa5c0ca1ab828369686c71cdcf65045895ae1c
DiegoL <diegol@DiegoLs-iMac-Pro.local>
2020-08-06 16:07:09 -0300

    criação dos assets

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import JellyScroll from './src/screens/JellyScroll';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => JellyScroll);
